a8f3917dd3c036a44af4ebb35cec96a7634bc94c
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2022-12-05 17:04:42 +0100

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Start now, today is already too late!</h1>
     </div>
   );
