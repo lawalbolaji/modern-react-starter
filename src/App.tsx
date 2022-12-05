ff56e36f6e4296d4291186bbdc64a7e30f4ff3db
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2022-12-05 16:38:35 +0100

    migrate to ts

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Start now, today is already too late!</h1>
+    </div>
+  );
+}
+
+export default App;
