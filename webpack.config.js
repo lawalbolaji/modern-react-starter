4e717f92376d3d3083e57e4f2741b2b10ef68535
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2022-12-05 14:35:17 +0100

    add ui dev tooling config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    filename: "[name].js",
+    publicPath: "http://localhost:8080/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env", "@babel/react"] },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    open: true,
+    port: 8080,
+  },
+};
