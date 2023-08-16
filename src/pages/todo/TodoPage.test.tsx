771ae2da3c3b658c0f5401c19ae47646ddcae0f1
lawalbolaji <lawalabdulrasheedbolaji@gmail.com>
2023-08-16 19:09:14 +0100

    test: add automated integration tests;
    - test users can create new todos
    - test users cannot create empty todos

--- src/pages/todo/TodoPage.test.tsx
+++ src/pages/todo/TodoPage.test.tsx
@@ -1,3 +1,43 @@
-it("should just pass", () => {
-  expect(1).toEqual(1);
+import { act, fireEvent, screen } from "@testing-library/react";
+import { renderWithProviders } from "../../test/utils/test_utils";
+import { TodoPage } from "./TodoPage";
+
+describe("todo app", () => {
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
+
+  test("fetches all todos on startup", async () => {
+    const { store } = renderWithProviders(<TodoPage />);
+    const todoIds = store.getState().todo.ids;
+
+    expect(todoIds).toHaveLength(0);
+  });
+
+  test("allows users to create new todo", async () => {
+    const { store } = renderWithProviders(<TodoPage />);
+    const input = screen.getByPlaceholderText("add new todo here and press enter...") as HTMLInputElement;
+    expect(input.value).toBe("");
+
+    await act(() => {
+      fireEvent.change(input, { target: { value: "add integration tests to this package" } });
+      fireEvent.keyDown(input, { key: "Enter", code: "Enter", charCode: 13 });
+    });
+
+    expect(input.value).toBe("");
+    expect(store.getState().todo.ids).toHaveLength(1);
+  });
+
+  test("does not allow users to create empty todos", async () => {
+    const { store } = renderWithProviders(<TodoPage />);
+    const input = screen.getByPlaceholderText("add new todo here and press enter...") as HTMLInputElement;
+    expect(input.value).toBe("");
+
+    await act(() => {
+      fireEvent.keyDown(input, { key: "Enter", code: "Enter", charCode: 13 });
+    });
+
+    expect(input.value).toBe("");
+    expect(store.getState().todo.ids).toHaveLength(0);
+  });
 });
